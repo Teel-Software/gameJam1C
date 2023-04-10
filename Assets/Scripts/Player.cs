812bdf4c767468f3b7012a116b2a48333179b98d
AnesVijay <2011andreyka@gmail.com>
2023-04-10 16:43:33 +0500

    some minor changes
    minor changes + trying to turn player to the movement side after movement process

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public readonly TankType type = TankType.Player;
-    public float moveSpeed = 5f;
+    public float moveSpeed;
     public Rigidbody2D rb;
     public Animator animator;
 
-    private void OnEnable() {
-        
+    void Awake()
+    {
+        moveSpeed = PaEManager.GetPlayer().Speed;
+        //animator.SetFloat("TurnSide", 0);
     }
 
     Vector2 movement;
+    //float turnSide;
 
-    // Update is called once per frame
     void Update() {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
@@ -21,9 +22,32 @@ public class Player : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+        //GetSideToTurn();
+
+        // if (!Input.anyKeyDown){
+        //     animator.SetFloat("TurnSide", turnSide);
+        // }
     }
 
     private void FixedUpdate() {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + (moveSpeed * Time.fixedDeltaTime * movement));
     }
+
+    // private void GetSideToTurn(){
+    //     if (animator.GetFloat("Horizonal") == -1) {
+    //         turnSide = 1.0f;
+    //     }
+    //     else if (animator.GetFloat("Horizonal") == 1) {
+    //         turnSide = 2.0f;
+    //     }
+
+    //     else if (animator.GetFloat("Vertical") == -1) {
+    //         turnSide = 3.0f;
+    //     }
+    //     else {// if (animator.GetFloat("Vertical") == 1){
+    //         turnSide = 0.0f;
+    //     }
+
+    //     //return 0.0f;
+    // }
 }
